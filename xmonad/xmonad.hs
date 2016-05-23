c15de5649159cd1b87902b16a1418a866425c8d8
Zach Smestad <zsmestad@fastmail.fm>
2016-05-23 11:37:17 -0500

    Add xmobar configuration

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,27 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders (smartBorders0)
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig (additionalKeys)
+import System.IO
 
-main = xmonad defaultConfig
-    { terminal    = "urxvt"
+main = do
+  xmproc <- spawnPipe "/usr/bin/xmobar"
+  xmonad $ defaultConfig
+    { terminal    = myTerminal
     , modMask     = mod4Mask
-    , borderWidth = 3
+    , borderWidth = myBorderWidth
+    , manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts . smartBorders $ layoutHook defaultConfig
+    , logHook = myLogHook xmproc
+    }
+
+myTerminal = "urxvt"
+myBorderWidth = 2
+
+myLogHook xmproc =
+  dynamicLogWithPP xmobarPP
+    { ppOutput = hPutStrLn xmproc
+    , ppTitle = xmobarColor "green" "" . shorten 50
     }
