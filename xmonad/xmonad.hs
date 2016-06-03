28fcf552c36db487620195df923ac929edd4779f
Zach Smestad <zsmestad@fastmail.fm>
2016-06-02 22:44:33 -0500

    Alright xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,29 +1,47 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders (smartBorders)
-import XMonad.Util.Run (spawnPipe)
+import XMonad.Layout.Tabbed
 import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe)
 import System.IO
 
 main = do
   xmproc <- spawnPipe "/usr/bin/xmobar"
   xmonad $ defaultConfig
     { terminal    = myTerminal
-    , modMask     = mod4Mask
+    , modMask     = myModMask
     , borderWidth = myBorderWidth
-    , manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts . smartBorders $ layoutHook defaultConfig
-    , logHook = myLogHook xmproc
-    }
+    , manageHook  = myManageHooks
+    , layoutHook  = myLayoutHook defaultConfig
+    , logHook     = myLogHook xmproc
+    } `additionalKeys` [
+     (( myModMask, xK_f), spawn "firefox")
+    ,(( myModMask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
+    ,(( myModMask, xK_b), sendMessage ToggleStruts)
+    ]
+
+-- Windows key
+myModMask = mod4Mask
 
 myTerminal = "urxvt"
 myBorderWidth = 2
+
+myLayoutHook config =
+  avoidStruts . smartBorders $ layoutHook defaultConfig ||| simpleTabbed
 
 myLogHook xmproc =
   dynamicLogWithPP xmobarPP
     { ppOutput = hPutStrLn xmproc
     , ppTitle = xmobarColor "green" "" . shorten 50
     }
+
+myManageHooks =
+    composeAll [ manageDocks
+               , isFullscreen --> doFullFloat
+               , manageHook defaultConfig
+    ]
 
 -- xscreensaver-command -lock
