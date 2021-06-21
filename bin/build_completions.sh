c795b3259bec6908c5c7784bce63478ed78da357
Zach Smestad <z.smestad@sportradar.com>
2021-06-21 18:16:53 -0500

    Do pip3 completions

--- bin/build_completions.sh
+++ bin/build_completions.sh
@@ -2,4 +2,4 @@
 func=~/.zsh/func
 [ "$(which just 2> /dev/null)" ] && just --completions zsh > "${func}/_just"
 [ "$(which kubectl 2> /dev/null)" ] && kubectl completion zsh > "${func}/_kubectl"
-[ "$(which pip 2> /dev/null)" ] && pip completion --zsh > "${func}/_pip"
+[ "$(which pip3 2> /dev/null)" ] && pip3 completion --zsh > "${func}/_pip3"
