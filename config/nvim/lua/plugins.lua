27cfb5bb3f82087e4b0174cd1ddcc1a72b21fbc8
Zach Smestad <zsmestad@fastmail.com>
2021-10-19 18:22:18 -0500

    Nvim: airline config

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -3,6 +3,9 @@ require('gitsigns').setup()
 
 -- Airline
 g['airline#extensions#ale#enabled'] = true
+g['airline#extensions#tabline#enabled'] = true
+g['airline#extensions#ale#enabled'] = true
+g.airline_powerline_fonts = true
 
 -- Alie
 g.ale_lint_on_text_changed = 'never'
@@ -14,11 +17,6 @@ g.ale_virtualtext_cursor = 1
 -- Deoplete
 g.python3_host_prog = '/usr/local/bin/python3'
 g['deoplete#enable_at_startup'] = true
-
--- VIM Airline
-g['airline#extensions#tabline#enabled'] = true
-g['airline#extensions#ale#enabled'] = true
-g.airline_powerline_fonts = true
 
 -- Markdown
 g.vim_markdown_folding_disabled = true
