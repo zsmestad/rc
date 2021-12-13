dbccc36dcab69cbeabf25c775536a221b6d01793
Zach Smestad <zsmestad@fastmail.com>
2021-12-13 08:20:10 -0600

    Nvim: add utils w/ map function

--- config/nvim/lua/maps.lua
+++ config/nvim/lua/maps.lua
@@ -1,26 +1,26 @@
-local map = vim.api.nvim_set_keymap
+local map = require("utils").map
 local g   = vim.g
 
-map('', ',', '<Nop>', { noremap = true, silent = true })
+map('', ',', '<Nop>', { silent = true })
 g.mapleader = ','
 g.maplocalleader = ','
 
-map('n', ',', '', {})
+map('n', ',', '')
 
 -- Move easily between windows
 -- iTerm2 issue: Set ^h to send escape code '[104;5u'
-map('n', '<C-h>', '<C-w>h', {})
-map('n', '<C-j>', '<C-w>j', {})
-map('n', '<C-k>', '<C-w>k', {})
-map('n', '<C-l>', '<C-w>l', {})
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
 
-map('n', '<C-n>', ':NERDTreeToggle<cr>', {})
-map('n', '<leader>n', ':NERDTreeToggle<cr>', {})
-map('n', '<leader>f', ':NERDTreeFind<cr>', {})
+map('n', '<C-n>', ':NERDTreeToggle<cr>')
+map('n', '<leader>n', ':NERDTreeToggle<cr>')
+map('n', '<leader>f', ':NERDTreeFind<cr>')
 
-map('n', '<leader>F', ':FZF<cr>', {})
+map('n', '<leader>F', ':FZF<cr>')
 
-map('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<cr>]], {})
-map('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], {})
-map('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers()<cr>]], {})
-map('n', '<leader>fh', [[<cmd>lua require('telescope.builtin').help_tags()<cr>]], {})
+map('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<cr>]])
+map('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]])
+map('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers()<cr>]])
+map('n', '<leader>fh', [[<cmd>lua require('telescope.builtin').help_tags()<cr>]])
