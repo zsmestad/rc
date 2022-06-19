6bdfefeb0d627b4e39c36af3556d33a9615ecd79
Zach Smestad <zsmestad@fastmail.com>
2022-06-19 18:41:15 -0500

    Nvim: comments

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,6 @@
 -- Based on [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim)
+-- ^ Repo deleted a few days after I used it, lol
+
 -- Install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
@@ -17,16 +19,21 @@ require('packer').startup(function(use)
   -- UI to select things (files, grep results, open buffers...)
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
   --use 'mjlbach/onedark.nvim' -- Theme inspired by Atom
   use 'morhetz/gruvbox'
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+
   -- Add git related info in the signs columns and popups
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+
   -- Highlight, edit, and navigate code using a fast incremental parsing library
   use 'nvim-treesitter/nvim-treesitter'
+
   -- Additional textobjects for treesitter
   use 'nvim-treesitter/nvim-treesitter-textobjects'
   use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+
   -- Complation
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-buffer'
