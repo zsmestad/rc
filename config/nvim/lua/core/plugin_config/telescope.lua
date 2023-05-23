74e13263d0f00ebc2e08d41a5c014d86e991c3da
Zach Smestad <zsmestad@fastmail.com>
2023-05-22 22:55:49 -0500

    Nvim: telescope nerd font v3, add keymap back

--- config/nvim/lua/core/plugin_config/telescope.lua
+++ config/nvim/lua/core/plugin_config/telescope.lua
@@ -1,3 +1,24 @@
+-- Telescope
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers)
+vim.keymap.set('n', '<leader>sf', function()
+  require('telescope.builtin').find_files { previewer = false }
+end)
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find)
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags)
+vim.keymap.set('n', '<leader>st', require('telescope.builtin').tags)
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').grep_string)
+vim.keymap.set('n', '<leader>sp', require('telescope.builtin').live_grep)
+vim.keymap.set('n', '<leader>so', function()
+  require('telescope.builtin').tags { only_current_buffer = true }
+end)
+
+local opts = { noremap = true }
+vim.keymap.set('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<cr>]], opts)
+vim.keymap.set('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], opts)
+vim.keymap.set('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers()<cr>]], opts)
+vim.keymap.set('n', '<leader>fh', [[<cmd>lua require('telescope.builtin').help_tags()<cr>]], opts)
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles)
+
 require('telescope').setup {
   defaults = {
     mappings = {
