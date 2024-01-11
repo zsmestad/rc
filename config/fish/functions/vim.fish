00c9e0d14286b7b92e749008661f266fc6a81227
Zach Smestad <z.smestad@sportradar.com>
2024-01-11 10:40:42 -0600

    Fish: work around Ubuntu magic

--- config/fish/functions/vim.fish
+++ config/fish/functions/vim.fish
@@ -1,3 +1,9 @@
 function vim
-  command nvim $argv; or command vim $argv
+  # Ubuntu magic doesn't like this:
+  # command nvim $argv; or command vim $argv
+  if command -q nvim
+    command nvim $argv
+  else
+    command vim $argv
+  end
 end
